d3bd8f5144b6d4d8bb18e322f17edd9c835ce373
marinos <mr.nisiotis@gmail.com>
2023-08-06 12:30:58 +0300

    Edit files, and add workflow in the project

--- morse/BinaryCodeToMorseCode/bitsToMorseCode.py
+++ morse/BinaryCodeToMorseCode/bitsToMorseCode.py
@@ -1,7 +1,8 @@
 # Bits to morse python file
 from BinaryCodeToMorseCode import convertBinaryToMorseSymbols
 
-def findTransmissionRate(pos, bit, morseString):
+
+def find_transmission_rate(pos, bit, morseString):
     size = 0
     for index, value in enumerate(morseString[pos:], pos):
         if value == bit:
@@ -10,27 +11,33 @@ def findTransmissionRate(pos, bit, morseString):
             break
     return size, index
 
-def convertBitsToDotsDashes(stringOfBits):
-    stringOfBits = stringOfBits.strip('0')  # remove possible zeroes from the begin and end of the string
+
+def convertBitsToDotsDashes(stringOfBits):  # this is wrong. Here I find the correct representation of the zeroes and ones
+    stringOfBits = stringOfBits.strip('0')  # remove possible zeroes from the beginning and end of the string
     decodedString = ""
-
     transmissionRate = 1   # the 'default' and correct transmission rate of the stringOfBits variable is: 1
-    sizeOfMorseBit, indexOfNextBit = findTransmissionRate(stringOfBits.find('1'), '1', stringOfBits)
 
+    sizeOfMorseBit, indexOfNextBit = find_transmission_rate(stringOfBits.find('1'), '1', stringOfBits)
     if sizeOfMorseBit == 2:
         transmissionRate = 2
 
-    elif sizeOfMorseBit>2: # if the size of the 1's bits is bigger than 2, I don't know if it has transmission rate or not e.g. if there are 3 1's at he beggining, is it dot'.' with tranmissionRate==3, or is it dash "-" which has 3 1's for decoding?
-        tempHelp = 0
-        sizeOfMorseBit2, indexOfNextBit2 = findTransmissionRate(indexOfNextBit, '0', stringOfBits)
+    elif sizeOfMorseBit >= 3:  # if the size of the 1's bits is bigger than 2, I don't know if it has transmission rate or not e.g. if there are 3 1's at he beginning, is it dot'.' with transmissionRate==3, or is it dash "-" which has 3 1's for decoding?
+        toggle = False
+        sizeOfMorseBit2, indexOfNextBit2 = find_transmission_rate(indexOfNextBit, '0', stringOfBits)
+        
         while sizeOfMorseBit == sizeOfMorseBit2:
-            toggle = bool(tempHelp % 2)
             bit = '0' if toggle else '1'
-            sizeOfMorseBit2, indexOfNextBit2 = findTransmissionRate(indexOfNextBit2, bit, stringOfBits)
-            tempHelp += 1
-        rate = sizeOfMorseBit / 3
-        if sizeOfMorseBit2 / rate == 1.0 or sizeOfMorseBit2 / rate == 7.0:
-            transmissionRate = int(rate)
+            sizeOfMorseBit2, indexOfNextBit2 = find_transmission_rate(indexOfNextBit2, bit, stringOfBits)
+            toggle = not toggle
+        
+        if sizeOfMorseBit2 == 0:
+            transmissionRate = sizeOfMorseBit
+        
+        elif sizeOfMorseBit > sizeOfMorseBit2:
+            transmissionRate = sizeOfMorseBit2
+        
+        elif sizeOfMorseBit < sizeOfMorseBit2:
+            transmissionRate = sizeOfMorseBit
 
     print("The transmissionRate of this string is: ", transmissionRate)
 
@@ -38,15 +45,16 @@ def convertBitsToDotsDashes(stringOfBits):
     zerosSubstring = transmissionRate * "0"
     onesSubstring = transmissionRate * "1"
     subString = ""
-    for i in range (0, len(stringOfBits), transmissionRate):
-        subString = stringOfBits[i:i+transmissionRate]  #from the current position of the index, until (:) the size of the transmissionRate ==> create a substring to check the bits
+    for i in range(0, len(stringOfBits), transmissionRate):
+        subString = stringOfBits[i:i+transmissionRate]  # from the current position of the index, until (:) the size of the transmissionRate ==> create a substring to check the bits
         if subString == onesSubstring:
             decodedString += "1"
         
         elif subString == zerosSubstring:
             decodedString += "0"
-     # Until here I find the proper bits representation of a coded morse code
-     
+    # Until here I find the proper bits representation of a coded morse code
+
+    print("The proper bits representation is {0}\n".format(decodedString))
     morseCodeString = convertBinaryToMorseSymbols.createProperMorseCodeString(decodedString)
     
     return morseCodeString
--- morse/cppToPythonModule/morseModule.cpp
+++ morse/cppToPythonModule/morseModule.cpp
@@ -6,7 +6,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(morse, m){  //'morse' is the shared object that is created and will be imported in the python file as 'morse' module
     m.doc() = "Create a Python module for morseDecoder";
     
-    py::class_<MorseDecoder>(m, "MorseDecoderString")  // 'MorseDecoderString' is the name of the class that will be used at the python file. So I can have different name for the .cpp class
+    py::class_<MorseDecoder>(m, "DecodeMorseCode")  // 'DecodeMorseCode' is the name of the class that will be used at the python file. So I can have different name for the .cpp class
       .def(py::init())
       .def("decodeMorse", &MorseDecoder::decodeMorse); // 'decodeMorse' is the function that will be used in python file. The name that I declare there is used at python
 }
